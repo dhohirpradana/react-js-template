e6098f2b5a448c2897f34ab51ff3f6d28a7cb886
dhohirpradana <dhohirkudus@gmail.com>
2022-07-11 11:34:55 +0700

    feat: update v2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload update.
+          Edit <code>src/App.js</code> and save to reload update v2.
         </p>
         <a
           className="App-link"
