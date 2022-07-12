34049a8381f9fc1bcd782287263794551a280aad
dhohirpradana <dhohirkudus@gmail.com>
2022-07-12 11:39:05 +0700

    v5

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload update v4.
+          Edit <code>src/App.js</code> and save to reload update v5.
         </p>
         <a
           className="App-link"
